381259eba79807106f28800a76df5365e1de1083
Junjie_Yu <yujunjie@zju.edu.cn>
2019-05-13 00:34:18 +0800

    使用Spring的IOC容器管理Bean

---
+++ LearnSpring/src/test/java/com/BeanTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanTest {
+
+    @Test
+    public void test(){
+        // 从ClassPath路径下找一个spring.xml文件，生成一个spring的上下文。
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        // 获取bean
+        Bean bean = context.getBean("bean", Bean.class); // getBean有多种重载方法，具体使用哪种参看源码
+        System.out.println("bean = " + bean);
+    }
+}
