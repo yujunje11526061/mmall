66ab30a02df39fca3f5dcd773bdf052d8f1e1cd1
Junjie_Yu <yujunjie@zju.edu.cn>
2019-05-15 22:41:11 +0800

    日志输出路径修改 windows控制台输出所需的字符集修改。

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -17,17 +18,17 @@ public interface UserMapper {
 
     int checkUsername(String username);
 
-    User selectLogin(String username, String md5Password);
-
     int checkEmail(String str);
 
-    int checkAnswer(String username, String question, String answer);
+    User selectLogin(@Param("username") String username, @Param("password")String password);
 
     String selectQuestionByUsername(String username);
 
-    int updatePasswordByUsername(String username, String md5Password);
+    int checkAnswer(@Param("username")String username, @Param("question")String question, @Param("answer")String answer);
 
-    int checkPassword(String md5EncodeUtf8, Integer id);
+    int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew")String passwordNew);
 
-    int checkEmailByUserId(String email, Integer id);
+    int checkPassword(@Param(value="password")String password,@Param("userId")Integer userId);
+
+    int checkEmailByUserId(@Param(value="email")String email,@Param(value="userId")Integer userId);
 }
